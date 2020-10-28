1a61dabe63dcad0e336a85b0a0c0438374f2826b
C. Emmanuel Onyema <cemmanuelonyema@gmail.com>
2020-10-28 04:37:12 +0100

    initial commit

---
+++ dist/js/script.js
@@ -0,0 +1,43 @@
+
+
+
+  // Smooth Scrolling
+  $(' div a').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        1000,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+
+  
+  // Smooth Scrolling
+  $('.cf a').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        1000,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+  
+  
